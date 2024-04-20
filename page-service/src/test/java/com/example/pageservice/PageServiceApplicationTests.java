6f7c9c72ae61458b7c9d30bfe155d6682dbd4199
XinrZhou <1352492197@qq.com>
2024-04-20 23:47:18 +0800

    feat: 页面管理

---
+++ page-service/src/test/java/com/example/pageservice/PageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
